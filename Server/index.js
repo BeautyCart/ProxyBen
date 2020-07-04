6bc8d71050ec4ba57f75a8347285502569abbf66
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2020-07-04 16:37:33 -0700

    refactored for docker

--- Server/index.js
+++ Server/index.js
@@ -10,9 +10,12 @@ const port = 3007;
 app.use('/:productId', express.static(__dirname + '/../client/'));
 app.use(cors());
 
-app.use('/photoGallery', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true}));
-app.use('/checkout', createProxyMiddleware({target: 'http://localhost:4000', changeOrigin: true}));
-app.use('/explorethis', createProxyMiddleware({target: 'http://localhost:3141', changeOrigin: true}));
+// app.use('/photoGallery', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true}));
+// app.use('/checkout', createProxyMiddleware({target: 'http://localhost:4000', changeOrigin: true}));
+// app.use('/explorethis', createProxyMiddleware({target: 'http://localhost:3141', changeOrigin: true}));
+app.use('/explorethis', createProxyMiddleware({target: 'http://54.242.63.221:3141/', changeOrigin: true}));
+
+
 
 app.listen(port, () => {console.log(`Server listening at http://localhost:${port}`)});
 
